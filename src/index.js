4e6ed25913589e1e6c2e3fc02a7fa96ab8a89a32
livalex <livalex11@gmail.com>
2016-11-10 02:01:07 +0300

    test 2B complete with full testing

--- src/index.js
+++ src/index.js
@@ -11,22 +11,48 @@ app.get('/', (req, res) => {
 
 app.get('/test2b', (req, res) => {
 
-	let data = req.query.fullname,
-		tmp = data.split(' '),
-		qualWords = tmp.length;
+	const digits = "0123456789",
+		  symbols = '!@#$%^&*()_+<>?/\=-';
 
-	switch(qualWords){
-		case 1:
-			res.send(`${tmp[0]}`);
+	let data = (req.query.fullname).toLowerCase(),
+		tmp = data.split(' '),
+		inputDataError = false;
+
+	for (let i = tmp.length - 1; i >= 0; i--) {
+		if (tmp[i] === '') {
+			tmp.splice(i,1);
+		}
+	}
+		
+	const qualWords = tmp.length;
+
+	for (let i = 0, l = data.length; i < l; i++){
+		if (digits.indexOf(data[i]) !== -1 || symbols.indexOf(data[i]) !== -1) {
+			inputDataError = true;
 			break;
-		case 2:
-			res.send(`${tmp[1]} ${tmp[0].charAt(0)}.`);
-			break;
-		case 3:
-			res.send(`${tmp[2]} ${tmp[0].charAt(0)}. ${tmp[1].charAt(0)}.`);
-			break;
-		default:
-			res.send('Invalid fullname');
+		}
+	}
+
+
+
+	if (data === '' || data === ' ' || inputDataError) {
+		res.send('Invalid fullname');		
+	} else {
+		switch(qualWords){
+			case 1:
+				res.send(`${tmp[0].charAt(0).toUpperCase()}${tmp[0].slice(1)}`);
+				break;
+			case 2:
+				res.send(`${tmp[1].charAt(0).toUpperCase()}${tmp[1].slice(1)} ${tmp[0].charAt(0).toUpperCase()}.`);
+				break;
+			case 3:
+				res.send(`${tmp[2].charAt(0).toUpperCase()}${tmp[2].slice(1)} ${tmp[0].charAt(0).toUpperCase()}. ${tmp[1].charAt(0).toUpperCase()}.`);
+				break;
+			default:
+				res.send('Invalid fullname');		
+	}
+
+
 	}
 })
 
